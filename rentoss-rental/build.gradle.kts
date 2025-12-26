d61d6ecb51920e7615f3ca0f0907155b94180f66
0ssang <dimple0513@naver.com>
2025-12-26 23:35:45 +0900

    chore: rental 모듈 구성

---
+++ rentoss-rental/build.gradle.kts
@@ -0,0 +1,26 @@
+dependencies {
+    implementation(project(":rentoss-core"))
+
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
+
+    runtimeOnly("org.postgresql:postgresql")
+
+    implementation("com.querydsl:querydsl-jpa:5.1.0:jakarta")
+    annotationProcessor("com.querydsl:querydsl-apt:5.1.0:jakarta")
+    annotationProcessor("jakarta.annotation:jakarta.annotation-api")
+    annotationProcessor("jakarta.persistence:jakarta.persistence-api")
+
+    implementation("org.springframework.cloud:spring-cloud-starter-openfeign")
+
+    implementation("org.springframework.kafka:spring-kafka")
+
+    implementation("org.redisson:redisson-spring-boot-starter:3.40.2")
+}
+
+dependencyManagement {
+    imports {
+        mavenBom("org.springframework.cloud:spring-cloud-dependencies:2025.0.0")
+    }
+}
