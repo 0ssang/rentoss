f36e933c2302bad2e97d803ada4e675e93b45291
0ssang <dimple0513@naver.com>
2025-12-26 22:34:35 +0900

    init: 멀티 모듈 프로젝트 설정

---
+++ build.gradle.kts
@@ -0,0 +1,54 @@
+plugins {
+    java
+    id("org.springframework.boot") version "3.5.9"
+    id("io.spring.dependency-management") version "1.1.7"
+}
+
+group = "com.rentoss"
+version = "0.0.1-SNAPSHOT"
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(21)
+    }
+}
+
+tasks.bootJar {enabled = false}
+tasks.jar {enabled = false}
+
+allprojects {
+    repositories {
+        mavenCentral()
+    }
+}
+
+subprojects {
+    apply(plugin = "java")
+    apply(plugin = "org.springframework.boot")
+    apply(plugin = "io.spring.dependency-management")
+
+    java {
+        toolchain {
+            languageVersion = JavaLanguageVersion.of(21)
+        }
+    }
+
+    configurations {
+        compileOnly {
+            extendsFrom(configurations.annotationProcessor.get())
+        }
+    }
+
+    dependencies {
+        compileOnly("org.projectlombok:lombok")
+        annotationProcessor("org.projectlombok:lombok")
+        annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
+
+        testImplementation("org.springframework.boot:spring-boot-starter-test")
+        testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    }
+
+    tasks.withType<Test> {
+        useJUnitPlatform()
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,13 @@
+rootProject.name = "rentoss"
+
+include(
+    "rentoss-core",
+    "rentoss-user",
+    "rentoss-item",
+    "rentoss-rental",
+    "rentoss-payment",
+    "rentoss-chat",
+    "rentoss-community",
+    "rentoss-notification",
+    "rentoss-gateway"
+)
