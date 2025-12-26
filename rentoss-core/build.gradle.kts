e7585b81e1fb8470047a6ba4e9beafda9df92cb1
0ssang <dimple0513@naver.com>
2025-12-26 23:08:31 +0900

    init: 공통 모듈 구성

---
+++ rentoss-core/build.gradle.kts
@@ -0,0 +1,8 @@
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
+}
+
+tasks.bootJar { enabled = false }
+tasks.jar { enabled = true }
