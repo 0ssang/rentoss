81847e013bb9d09408ca9cf71c6c7776a4967964
0ssang <dimple0513@naver.com>
2025-12-30 01:57:32 +0900

    feat: User 도메인 엔티티 및 Repository 구현

---
+++ rentoss-user/src/main/java/com/rentoss/user/domain/SocialProvider.java
@@ -0,0 +1,7 @@
+package com.rentoss.user.domain;
+
+public enum SocialProvider {
+    KAKAO,
+    NAVER,
+    GOOGLE
+}
---
+++ rentoss-user/src/main/java/com/rentoss/user/domain/User.java
@@ -0,0 +1,75 @@
+package com.rentoss.user.domain;
+
+import com.rentoss.core.domain.BaseEntity;
+import com.rentoss.core.domain.Location;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "users", schema = "user_schema")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class User extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String socialId;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private SocialProvider provider;
+
+    private String email;
+
+    @Column(nullable = false)
+    private String nickname;
+
+    private String profileImageUrl;
+
+    @Embedded
+    private Location location;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private UserStatus status;
+
+    @Builder(access = AccessLevel.PRIVATE)
+    private User(String socialId, SocialProvider provider, String email, String nickname, String profileImageUrl) {
+        this.socialId = socialId;
+        this.provider = provider;
+        this.email = email;
+        this.nickname = nickname;
+        this.profileImageUrl = profileImageUrl;
+        this.status = UserStatus.ACTIVE;
+    }
+
+    public static User create(String socialId, SocialProvider provider, String email, String nickname, String profileImageUrl) {
+        return User.builder()
+                .socialId(socialId)
+                .provider(provider)
+                .email(email)
+                .nickname(nickname)
+                .profileImageUrl(profileImageUrl)
+                .build();
+    }
+
+    public void updateProfile(String nickname, String profileImageUrl) {
+        this.nickname = nickname;
+        this.profileImageUrl = profileImageUrl;
+    }
+
+    public void updateLocation(Location location) {
+        this.location = location;
+    }
+
+    public void withdraw(){
+        this.status = UserStatus.WITHDRAWN;
+    }
+
+}
