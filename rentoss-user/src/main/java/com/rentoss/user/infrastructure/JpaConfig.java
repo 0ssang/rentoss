d9e40b2979b136bdfddc299496b0987461bfc49c
Youngsang Cho <51320595+0ssang@users.noreply.github.com>
2025-12-30 00:06:37 +0900

    [User] 모듈 기본 설정 (#3)

---
+++ rentoss-user/src/main/java/com/rentoss/user/infrastructure/JpaConfig.java
@@ -0,0 +1,9 @@
+package com.rentoss.user.infrastructure;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaConfig {
+}
