e0adf8e6d0cb7588ab87db77647db2b075a8f2c8
Youngsang Cho <51320595+0ssang@users.noreply.github.com>
2025-12-30 18:18:47 +0900

    [USER] 도메인 엔티티 구현 (#4)
    * feat: User 도메인 엔티티 및 Repository 구현
    
    * feat: User 에러 코드 구현

---
+++ rentoss-user/src/main/java/com/rentoss/user/exception/UserErrorCode.java
@@ -0,0 +1,19 @@
+package com.rentoss.user.exception;
+
+import com.rentoss.core.exception.ErrorCode;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@RequiredArgsConstructor
+public enum UserErrorCode implements ErrorCode {
+
+    USER_NOT_FOUND(HttpStatus.NOT_FOUND, "U001", "사용자를 찾을 수 없습니다."),
+    DUPLICATE_NICKNAME(HttpStatus.CONFLICT, "U002", "이미 사용중인 닉네임 입니다."),
+    ALREADY_WITHDRAWN(HttpStatus.BAD_REQUEST, "U003", "이미 탈퇴한 사용자 입니다.");
+
+    private final HttpStatus status;
+    private final String code;
+    private final String message;
+}
