91718ba08cad6d69ee75ea776f8a39d8306a35df
0ssang <dimple0513@naver.com>
2025-12-31 15:28:12 +0900

    chore: core 모듈 QueryDSL 의존성 추가

--- rentoss-core/build.gradle.kts
+++ rentoss-core/build.gradle.kts
@@ -2,6 +2,12 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-validation")
+
+    // QueryDSL
+    implementation("com.querydsl:querydsl-jpa:5.1.0:jakarta")
+    annotationProcessor("com.querydsl:querydsl-apt:5.1.0:jakarta")
+    annotationProcessor("jakarta.annotation:jakarta.annotation-api")
+    annotationProcessor("jakarta.persistence:jakarta.persistence-api")
 }
 
 tasks.bootJar { enabled = false }
