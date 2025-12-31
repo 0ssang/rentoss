bfe23e71e5ccf9cfa77d55b04b960481ba4c6c60
0ssang <dimple0513@naver.com>
2025-12-31 17:13:45 +0900

    feat: 탈퇴 시 회원 상태 검증 로직 추가

--- rentoss-user/src/main/java/com/rentoss/user/domain/User.java
+++ rentoss-user/src/main/java/com/rentoss/user/domain/User.java
@@ -2,6 +2,8 @@ package com.rentoss.user.domain;
 
 import com.rentoss.core.domain.BaseEntity;
 import com.rentoss.core.domain.Location;
+import com.rentoss.core.exception.BusinessException;
+import com.rentoss.user.exception.UserErrorCode;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Builder;
@@ -69,7 +71,9 @@ public class User extends BaseEntity {
     }
 
     public void withdraw(){
+        if (this.status == UserStatus.ACTIVE) {
+            throw new BusinessException(UserErrorCode.ALREADY_WITHDRAWN);
+        }
         this.status = UserStatus.WITHDRAWN;
     }
-
 }
