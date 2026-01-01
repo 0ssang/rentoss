082d812abbfbf9d367b7105dd9b30dcf02d1fad6
0ssang <dimple0513@naver.com>
2026-01-01 23:45:07 +0900

    chore: 각 서비스 별 Application 클래스 생성

---
+++ rentoss-notification/src/main/java/com/rentoss/notification/NotificationApplication.java
@@ -0,0 +1,9 @@
+package com.rentoss.notification;
+
+@SpringBootApplication
+public class NotificationApplication {
+
+    public static void main(String[] args){
+        SpringApplication.run(NotificationApplication.class, args);
+    }
+}
