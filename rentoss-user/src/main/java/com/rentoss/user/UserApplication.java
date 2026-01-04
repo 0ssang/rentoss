678c792d3daf6026f24818804a268843eef2f437
0ssang <dimple0513@naver.com>
2026-01-05 00:57:54 +0900

    chore: Application 클래스에 core 모듈을 스캔하도록 설정

--- rentoss-user/src/main/java/com/rentoss/user/UserApplication.java
+++ rentoss-user/src/main/java/com/rentoss/user/UserApplication.java
@@ -2,8 +2,10 @@ package com.rentoss.user;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
-@SpringBootApplication
+@ConfigurationPropertiesScan("com.rentoss.core")
+@SpringBootApplication(scanBasePackages = "com.rentoss")
 public class UserApplication {
 
     public static void main(String[] args){
