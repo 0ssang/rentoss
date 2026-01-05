2875eaf5d55b3b949b968f7927e7bb3e5b016a0f
0ssang <dimple0513@naver.com>
2026-01-05 22:39:30 +0900

    chore: spring boot test 의존성 추가

--- rentoss-core/build.gradle.kts
+++ rentoss-core/build.gradle.kts
@@ -2,6 +2,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-validation")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
 
     // QueryDSL
     implementation("com.querydsl:querydsl-jpa:5.1.0:jakarta")
