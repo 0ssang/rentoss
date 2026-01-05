a76ff6c4783d2c443811a82c20d17ec02227046d
0ssang <dimple0513@naver.com>
2026-01-05 19:20:27 +0900

    refactor: CurrentUserInfo 필드 추가

--- rentoss-core/src/main/java/com/rentoss/core/domain/model/CurrentUserInfo.java
+++ rentoss-core/src/main/java/com/rentoss/core/domain/model/CurrentUserInfo.java
@@ -1,10 +1,15 @@
 package com.rentoss.core.domain.model;
 
+import com.rentoss.core.domain.enums.UserRole;
+
 public record CurrentUserInfo(
         Long userId,
-        String role
+        String email,
+        String nickname,
+        String profileImageUrl,
+        UserRole role
 ) {
-    public static CurrentUserInfo of(Long userId, String role) {
-        return new CurrentUserInfo(userId, role);
+    public static CurrentUserInfo of(Long userId, String email, String nickname, String profileImageUrl, UserRole role) {
+        return new CurrentUserInfo(userId, email, nickname, profileImageUrl, role);
     }
 }
