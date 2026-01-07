faaf24b74a45f7c530fad3ea108eaefdf8dfad1e
Youngsang Cho <51320595+0ssang@users.noreply.github.com>
2026-01-07 21:06:05 +0900

    [core] CookieUtils 구현 (#15)
    * feat: CookieUtils 구현
    
    * test: CookieUtils 테스트 구현
    
    * refactor: 삭제 쿠키 생성 시 기존 로직 활용하도록 수정

---
+++ rentoss-core/src/main/java/com/rentoss/core/security/cookie/CookieUtils.java
@@ -0,0 +1,57 @@
+package com.rentoss.core.security.cookie;
+
+import com.rentoss.core.security.jwt.JwtProperties;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseCookie;
+import org.springframework.stereotype.Component;
+
+import java.time.Duration;
+
+@Component
+@RequiredArgsConstructor
+public class CookieUtils {
+
+    private final JwtProperties jwtProperties;
+
+    public ResponseCookie createAccessTokenCookie(String token) {
+        return createCookie(
+                jwtProperties.getAccessTokenCookieName(),
+                token,
+                jwtProperties.getAccessTokenExpiration()
+        );
+    }
+
+    public ResponseCookie createRefreshTokenCookie(String token) {
+        return createCookie(
+                jwtProperties.getRefreshTokenCookieName(),
+                token,
+                jwtProperties.getRefreshTokenExpiration()
+        );
+    }
+
+    private ResponseCookie createCookie(String name, String value, Duration maxAge) {
+        return ResponseCookie.from(name, value)
+                .httpOnly(true)
+                .secure(jwtProperties.isCookieSecure())
+                .path("/")
+                .maxAge(maxAge)
+                .sameSite("Lax")
+                .build();
+    }
+
+    public ResponseCookie deleteAccessTokenCookie() {
+        return createCookie(
+                jwtProperties.getAccessTokenCookieName(),
+                "",
+                Duration.ZERO
+        );
+    }
+
+    public ResponseCookie deleteRefreshTokenCookie() {
+        return createCookie(
+                jwtProperties.getRefreshTokenCookieName(),
+                "",
+                Duration.ZERO
+        );
+    }
+}
