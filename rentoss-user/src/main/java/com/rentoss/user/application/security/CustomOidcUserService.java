11f5777a70f0c51181e54fd9654c74777ee60f0e
0ssang <dimple0513@naver.com>
2026-01-11 23:23:28 +0900

    feat: 커스텀 OIDC 서비스 구현

--- rentoss-user/src/main/java/com/rentoss/user/application/security/CustomOidcUserService.java
+++ rentoss-user/src/main/java/com/rentoss/user/application/security/CustomOidcUserService.java
@@ -3,68 +3,122 @@ package com.rentoss.user.application.security;
 import com.rentoss.user.domain.SocialProvider;
 import com.rentoss.user.domain.User;
 import com.rentoss.user.domain.UserRepository;
-import com.rentoss.user.domain.oauth.GoogleUserInfo;
-import com.rentoss.user.domain.oauth.KakaoUserInfo;
-import com.rentoss.user.domain.oauth.NaverUserInfo;
-import com.rentoss.user.domain.oauth.OAuth2UserInfo;
+import com.rentoss.user.domain.oauth.*;
 import com.rentoss.user.domain.principal.UserPrincipal;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.oauth2.client.registration.ClientRegistration;
 import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserRequest;
 import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserService;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
 import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.client.RestClient;
+import org.springframework.web.client.RestTemplate;
 
+import java.util.HashMap;
 import java.util.Map;
 
 @Service
 @RequiredArgsConstructor
 @Slf4j
 public class CustomOidcUserService extends OidcUserService {
+
     private final UserRepository userRepository;
+    private final RestClient restClient = RestClient.create();
 
     @Override
     @Transactional
     public OidcUser loadUser(OidcUserRequest userRequest) throws OAuth2AuthenticationException {
+        ClientRegistration clientRegistration = userRequest.getClientRegistration();
+        String registrationId = clientRegistration.getRegistrationId();
+
+        String userInfoUri = clientRegistration.getProviderDetails().getUserInfoEndpoint().getUri();
+        if ("naver".equals(registrationId)) {
+            return processNaverUser(userRequest, userInfoUri);
+        }
+
+        return processStandardUser(userRequest);
+    }
+
+    private OidcUser processStandardUser(OidcUserRequest userRequest) {
         OidcUser oidcUser = super.loadUser(userRequest);
-        log.info("OIDC Attributes: {}", oidcUser.getAttributes());
+        return processUserRegistration(
+                userRequest.getClientRegistration().getRegistrationId(),
+                oidcUser.getAttributes(),
+                oidcUser
+        );
+    }
+
+    private OidcUser processNaverUser(OidcUserRequest userRequest, String userInfoUri) {
+        ClientRegistration clientRegistration = ClientRegistration.withClientRegistration(userRequest.getClientRegistration())
+                .userInfoUri(null)
+                .build();
+
+        OidcUserRequest newRequest = new OidcUserRequest(
+                clientRegistration,
+                userRequest.getAccessToken(),
+                userRequest.getIdToken(),
+                userRequest.getAdditionalParameters()
+        );
+
+        OidcUser oidcUser = super.loadUser(newRequest);
+
+        Map<String, Object> mergedAttributes = fetchNaverUserInfo(userRequest, oidcUser.getAttributes(), userInfoUri);
 
         String registrationId = userRequest.getClientRegistration().getRegistrationId();
-        OAuth2UserInfo userInfo = extractUserInfo(registrationId, oidcUser.getAttributes());
 
+        return processUserRegistration(registrationId, mergedAttributes, oidcUser);
+    }
+
+    private OidcUser processUserRegistration(String registrationId, Map<String, Object> attributes, OidcUser oidcUser) {
+        log.info(" OIDC Attributes for {}: {}", registrationId, attributes);
+        OAuth2UserInfo userInfo = OAuth2UserInfoFactory.getOAuth2UserInfo(registrationId, attributes);
         User user = saveOrUpdate(userInfo);
-
         return new UserPrincipal(user, oidcUser);
     }
 
-    private OAuth2UserInfo extractUserInfo(String registrationId, Map<String, Object> attributes) {
-        return switch (registrationId) {
-            case "google" -> new GoogleUserInfo(attributes);
-            case "kakao" -> new KakaoUserInfo(attributes);
-            case "naver" -> new NaverUserInfo(attributes);
-            default -> throw new OAuth2AuthenticationException("지원하지 않는 provider 입니다: "+ registrationId);
-        };
+    private Map<String, Object> fetchNaverUserInfo(OidcUserRequest userRequest, Map<String, Object> originalAttributes, String userInfoUri) {
+        try {
+            Map<String, Object> response = restClient.get()
+                    .uri(userInfoUri)
+                    .header(HttpHeaders.AUTHORIZATION, "Bearer " + userRequest.getAccessToken().getTokenValue())
+                    .retrieve()
+                    .body(new ParameterizedTypeReference<>() {
+                    });
+
+            Map<String, Object> mergedAttributes = new HashMap<>(originalAttributes);
+            if(response != null) {
+                mergedAttributes.putAll(response);
+            }
+            return mergedAttributes;
+        } catch (Exception e) {
+            log.error("failed to fetch Naver UserInfo", e);
+            throw new OAuth2AuthenticationException("Failed to get user info from Naver");
+        }
     }
 
     private User saveOrUpdate(OAuth2UserInfo userInfo) {
         SocialProvider provider = SocialProvider.valueOf(userInfo.getProvider().toUpperCase());
 
-        return userRepository.findBySocialIdAndProvider(userInfo.getProviderId(), provider)
+        User user = userRepository.findBySocialIdAndProvider(userInfo.getProviderId(), provider)
                 .map(entity -> {
                     entity.updateProfile(userInfo.getNickname(), userInfo.getProfileImageUrl());
                     return entity;
                 })
-                .orElseGet(() -> {
-                    User newUser = User.create(
-                            userInfo.getProviderId(),
-                            provider,
-                            userInfo.getEmail(),
-                            userInfo.getEmail(),
-                            userInfo.getProfileImageUrl()
-                    );
-                    return userRepository.save(newUser);
-                });
+                .orElse(User.create(
+                        userInfo.getProviderId(),
+                        provider,
+                        userInfo.getEmail(),
+                        userInfo.getNickname(),
+                        userInfo.getProfileImageUrl()
+                ));
+        return userRepository.save(user);
     }
 }
