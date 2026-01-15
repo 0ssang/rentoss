2003d2dff18981308d79705d11459d849cb440d8
Youngsang Cho <51320595+0ssang@users.noreply.github.com>
2026-01-16 00:57:42 +0900

    [gateway] 글로벌 Jwt 인증 필터 구현 (#17)
    * chore: gateway 의존성 설정
    
    * chore: 내부 서비스 라우터 설정
    
    * feat: 글로벌 Jwt 인증 필터 구현
    
    * chore: 스프링부트 버전 다운그레이드
    
    * test: 글로벌 Jwt 인증 필터 통합테스트

--- rentoss-gateway/build.gradle.kts
+++ rentoss-gateway/build.gradle.kts
@@ -1,14 +1,16 @@
 dependencies {
+    implementation(project(":rentoss-core")) {
+        exclude(group = "org.springframework.boot", module = "spring-boot-starter-web")
+    }
     implementation("org.springframework.cloud:spring-cloud-starter-gateway")
     implementation("org.springframework.boot:spring-boot-starter-data-redis-reactive")
 
-    implementation("io.jsonwebtoken:jjwt-api:0.12.6")
-    runtimeOnly("io.jsonwebtoken:jjwt-impl:0.12.6")
-    runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.12.6")
+    testImplementation("org.springframework.cloud:spring-cloud-starter-contract-stub-runner")
+    testImplementation("io.projectreactor:reactor-test")
 }
 
 dependencyManagement {
     imports {
-        mavenBom("org.springframework.cloud:spring-cloud-dependencies:2025.0.0")
+        mavenBom("org.springframework.cloud:spring-cloud-dependencies:2024.0.0")
     }
 }
